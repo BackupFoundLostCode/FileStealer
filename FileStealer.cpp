1e32c18a445e80cad3e3defb0dce4870ea6bad74
BackupFoundLostCode <maurice.oehl@gmail.com>
2025-09-20 18:56:32 +0200

    Update FileStealer.cpp

--- FileStealer.cpp
+++ FileStealer.cpp
@@ -11,14 +11,15 @@ using namespace std;
 int main() {
 	int option;
 	int* aRev = &option;
+	bool status = false;
 	std::string filenameWhichGetCopy;
-	std::string a = "FileStealerInfo.txt";
+	std::string  stealerTXT= "FileStealerInfo.txt";//a
 	std::string nameList = "NameList.txt";
-	std::string deviceLetter = "D:/";
-	std::string fusion = deviceLetter + a;
+	std::string deviceLetter = "D:\\"; //D: ?
+	std::string fusion = deviceLetter + stealerTXT;
 	std::string createNameList = deviceLetter + nameList;
 	std::ofstream listFile(createNameList, ios::app);
-	std::ofstream newFile(deviceLetter + a, ios::app);
+	std::ofstream newFile(fusion, ios::app);
 	std::filesystem::path forShowInfos(fusion);
 	std::filesystem::path ListOnDevice(createNameList);
 
@@ -37,131 +38,21 @@ int main() {
 		else {
 			switch (*aRev) {
 			case 1: {
-				std::cin.ignore(50, '\n');
-				std::cout << "Name for File to Copy:\n";
-				std::getline(std::cin, filenameWhichGetCopy);
-				//
-				std::filesystem::path PathCopyTo(deviceLetter);
-				std::filesystem::path p(deviceLetter + a);
-				try {
-					if (filesystem::exists(p)) {
-						cout << "\nPath on Device exists! Path is:\n";
-						cout << "\n";
-						for (const auto& path : p) {
-							cout << path << std::endl;
-						}
-						system("pause");
-						try {
-							setColor(10);
-							for (const auto& dd : std::filesystem::recursive_directory_iterator("C:/", std::filesystem::directory_options::skip_permission_denied | std::filesystem::directory_options::follow_directory_symlink)) {
-								//cout << dd << std::endl;
-								if (dd.path().string().find(filenameWhichGetCopy) != std::string::npos) {
-									cout << "\nFOUND FILE!\n";
-									cout << dd.path() << std::endl;
-									try {
-										std::filesystem::copy_file(dd.path(), PathCopyTo / dd.path().filename());
-										cout << "COPY SUCCESS!\n";
-										inputFile(newFile ,filenameWhichGetCopy,dd);
-									}
-									catch (const std::exception&) {
-										setColor(12);
-										cout << "Copy failed! Find information in FileStealer.txt!\n";
-										inputFile(newFile,filenameWhichGetCopy,dd);
-									}
-									break;
-								}
-							}
-						}
-						catch (const std::exception&) {
-							setColor(12);
-							cout << "Search failed or file does not exist.\n";
-							system("pause");
-							std::cout << "\n";
-						}
-					}
-					else {
-						setColor(12);
-						std::cout << "Path does not exist!\n";
-						system("pause");
-					}
-				}
-				catch (const std::exception&) {
-					setColor(12);
-					std::cout << "Search failed - fatal error!\n";
-					system("pause");
-				}
+				caseNumOne(filenameWhichGetCopy,deviceLetter,fusion,status,newFile);
 			}
 				break;
 			case 2: {
-				std::cin.ignore(50, '\n');
-				std::ifstream dataTXT{ forShowInfos };//Path for Output on console.
-				if (dataTXT.fail() || !dataTXT.is_open()) {
-					setColor(12);
-					std::cout << "Fail open FileStealerInfo.txt!\n";
-					setColor(10);
-					std::cout << "Back to Menue in 2 Seconds.\n";
-					std::cout << "\n";
-					Sleep(2000);
-					continue;
-				}
-				else {
-					std::string output;
-					setColor(10);
-					while (getline(dataTXT, output)) {
-						std::cout << output << std::endl;
-					}
-					dataTXT.close();
-					system("pause");
-				}
+				caseNumbTow(forShowInfos);
 			}
 				break;
 			case 3: {
-				std::cin.ignore(50, '\n');
-				std::cout << "Edit the List." << "\n" << std::endl;
-				std::ifstream dataList{ ListOnDevice };
-				std::string output;
-				//
-				std::filesystem::path PathCopyTo(deviceLetter);
-				std::filesystem::path p(deviceLetter + a);
-				setColor(10);
-				while (getline(dataList, output)) {
-					try {
-						setColor(10);
-						for (const auto& lol : std::filesystem::recursive_directory_iterator("C:/", std::filesystem::directory_options::skip_permission_denied | std::filesystem::directory_options::follow_directory_symlink)) {
-							if (lol.path().string().find(output) != std::string::npos) {
-								cout << "\nFOUND FILE!\n";
-								cout << lol.path() << std::endl;
-								try {
-									std::filesystem::copy_file(lol.path(), PathCopyTo / lol.path().filename());
-									cout << "COPY SUCCESS!\n";
-									inputFile(newFile,output,lol);
-								}
-								catch (const std::exception&) {
-									setColor(12);
-									cout << "Copy failed! Find information in FileStealer.txt!\n";
-									inputFile(newFile,output,lol);
-								}
-								break;
-							}
-						}
-					}
-					catch (const std::exception&) {
-						setColor(12);
-						cout << "Search failed or file does not exist.\n";
-						Sleep(2500);
-						std::cout << "\n";
-					}
-				}
-				dataList.close();
-				system("pause");
+				caseNumbThree(ListOnDevice, deviceLetter,stealerTXT,status,newFile);
 			}
 				break;
 			case 4: {
-				std::cin.ignore(50, '\n');
-				setColor(10);
-				std::cout << "Exit Program in 3 Seconds.";
-				Sleep(3000);
-				return 0;
+				if (caseNumbFour() == 0) {
+					return 0;
+				}
 			}
 				break;
 			default: setColor(12);
@@ -172,4 +63,3 @@ int main() {
 	}
 	return EXIT_SUCCESS;
 }
-
